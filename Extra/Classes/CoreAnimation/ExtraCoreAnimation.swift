849b244741fd12d9cebb66af48f36b0804a83e73
Guillaume Bonnin <guillaume@smartnsoft.com>
2018-10-12 17:57:17 +0200

    Extends CALayer as extra base

--- Extra/Classes/CoreAnimation/ExtraCoreAnimation.swift
+++ Extra/Classes/CoreAnimation/ExtraCoreAnimation.swift
@@ -20,26 +20,6 @@
 // OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 // SOFTWARE.
 
-extension Extra where Base: CALayer {
+import Foundation
 
-  /// Reproduces and applies a shadow that nearly perfectly matches the design according to Zeplin or Sketch parameters.
-  public func applySketchShadow(
-    color: UIColor = .black,
-    alpha: Float = 0.5,
-    x: CGFloat = 0,
-    y: CGFloat = 2,
-    blur: CGFloat = 4,
-    spread: CGFloat = 0) {
-    self.base.shadowColor = color.cgColor
-    self.base.shadowOpacity = alpha
-    self.base.shadowOffset = CGSize(width: x, height: y)
-    self.base.shadowRadius = blur / 2.0
-    if spread == 0 {
-      self.base.shadowPath = nil
-    } else {
-      let deltaX = -spread
-      let rect = self.base.bounds.insetBy(dx: deltaX, dy: deltaX)
-      self.base.shadowPath = UIBezierPath(rect: rect).cgPath
-    }
-  }
-}
+extension CALayer: ExtraCompatible { }
